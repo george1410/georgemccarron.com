f2c5f7dd03842e89d7a6e0b5f9c75e14f8479d21
George McCarron <hello@georgemccarron.com>
2023-12-01 22:03:57 +0000

    header background

--- src/components/GlobalNav.tsx
+++ src/components/GlobalNav.tsx
@@ -3,17 +3,19 @@ import { GlobalNavItem } from './GlobalNavItem';
 
 export const GlobalNav = () => {
   return (
-    <header className='max-w-screen-xl w-full self-center'>
-      <nav className=' m-4 flex justify-between font-medium text-zinc-500'>
-        <Link href='/' className='hover:text-zinc-900'>
-          George McCarron
-        </Link>
-        <ul className='flex gap-6 justify-end'>
-          <GlobalNavItem href='/'>Home</GlobalNavItem>
-          <GlobalNavItem href='/blog'>Blog</GlobalNavItem>
-          <GlobalNavItem href='/speaking'>Speaking</GlobalNavItem>
-        </ul>
-      </nav>
-    </header>
+    <div className='bg-zinc-100 flex justify-center sticky top-0 z-10'>
+      <header className='max-w-screen-xl w-full self-center'>
+        <nav className=' m-4 flex justify-between font-medium text-zinc-500'>
+          <Link href='/' className='text-zinc-900'>
+            George McCarron
+          </Link>
+          <ul className='flex gap-6 justify-end'>
+            <GlobalNavItem href='/'>Home</GlobalNavItem>
+            <GlobalNavItem href='/blog'>Blog</GlobalNavItem>
+            <GlobalNavItem href='/speaking'>Speaking</GlobalNavItem>
+          </ul>
+        </nav>
+      </header>
+    </div>
   );
 };
