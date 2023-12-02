e444b6990020f87e1963e75db813dbd90929d45e
George McCarron <hello@georgemccarron.com>
2023-12-02 01:49:08 +0000

    change button style

--- src/app/blog/[slug]/page.tsx
+++ src/app/blog/[slug]/page.tsx
@@ -63,7 +63,7 @@ export default function Page({ params }: { params: { slug: string } }) {
         <AboutAuthor />
         <Link
           href='/blog'
-          className='self-center border-2 border-slate-200 py-2 px-4 rounded-lg font-medium text-slate-400 hover:bg-slate-100 w-full md:w-auto text-center mt-4 md:mt-0'
+          className='self-center py-2 px-4 rounded-lg font-medium text-slate-400 hover:bg-slate-100 w-full md:w-auto text-center mt-4 md:mt-0'
         >
           Back to All Posts
         </Link>
