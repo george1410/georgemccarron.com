91b49fea21d0fbe33c990bb95fafd64fbe6f7dd6
George McCarron <hello@georgemccarron.com>
2024-01-21 18:24:35 +0000

    prettier

--- src/env.ts
+++ src/env.ts
@@ -1,5 +1,5 @@
-import { createEnv } from "@t3-oss/env-nextjs";
-import { z } from "zod";
+import { createEnv } from '@t3-oss/env-nextjs';
+import { z } from 'zod';
 
 export const env = createEnv({
   /*
@@ -10,7 +10,7 @@ export const env = createEnv({
     SPOTIFY_CLIENT_ID: z.string().min(1),
     SPOTIFY_CLIENT_SECRET: z.string().min(1),
     SPOTIFY_REFRESH_TOKEN: z.string().min(1),
-    NODE_ENV: z.enum(["development", "production"]),
+    NODE_ENV: z.enum(['development', 'production']),
   },
   /*
    * Environment variables available on the client (and server).
--- src/lib/posts.ts
+++ src/lib/posts.ts
@@ -1,6 +1,6 @@
-import { env } from "@/env";
-import { allPosts } from "contentlayer/generated";
+import { env } from '@/env';
+import { allPosts } from 'contentlayer/generated';
 
 export const publishedPosts = allPosts.filter((post) =>
-  env.NODE_ENV === "development" ? true : !post.draft
+  env.NODE_ENV === 'development' ? true : !post.draft
 );
